d8268cce9d1e4f7e407bdad1a60cf79d2ed3a1fb
Akira Shinohara <k017c1067@it-neec.jp>
2020-07-15 17:08:11 +0900

    feat: create discord bot

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/twinkling-gecko/brave-gecko
 
 go 1.14
+
+require github.com/bwmarrin/discordgo v0.21.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token string = os.Getenv("DISCORD_TOKEN")
+)
 
 func main() {
-	fmt.Println("Yes, brave-gecko.")
+	app, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	err = app.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	app.Close()
 }
